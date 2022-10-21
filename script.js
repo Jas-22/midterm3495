d4981e6bb8edf45a410ddbbbc7f1b398c687453b
Jaskirat Singh <jaskiratsingh772@gmail.com>
2022-10-21 17:30:06 -0400

    completed

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+
+let modal = document.querySelector(".modal-wrapper");
+let btn = document.querySelector(".modal-button");
+let close_btn = document.querySelector(".modal-close");
+let btnOk = document.querySelector(".modal-button-close");
+
+
+btnOk.addEventListener("click", hide);
+
+btn.addEventListener("click", display);
+
+
+function display() {
+  modal.style.display = "block";
+}
+
+//  when the user clicks on X button,close the modal
+close_btn.addEventListener("click", hide);
+
+// when user clicks anywhere outside the modal, close modal
+window.addEventListener("click", (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
+
+function hide() {
+  modal.style.display = "none";
+}
+
+
